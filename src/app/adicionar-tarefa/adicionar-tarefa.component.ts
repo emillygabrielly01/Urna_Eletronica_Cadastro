9dc4afc6d3b6bfcbad429709831d7a5943c3b494
emillygabrielly01 <emilly.silva2@stellantis.com>
2025-02-18 13:24:08 -0300

    Ajustando a Lista de Tarefas

--- src/app/adicionar-tarefa/adicionar-tarefa.component.ts
+++ src/app/adicionar-tarefa/adicionar-tarefa.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-adicionar-tarefa',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './adicionar-tarefa.component.html',
   styleUrl: './adicionar-tarefa.component.scss'
 })
-export class AdicionarTarefaComponent {
-
+export class AdicionarTarefaComponent 
+{
+  
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,16 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { AdicionarTarefaComponent } from "./adicionar-tarefa/adicionar-tarefa.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatIconModule, FormsModule, NgFor],
+  imports: [MatIconModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  tarefas: { tarefa: string, descricao: string, concluida: boolean }[] = [];
-  novaTarefa: { tarefa: string, descricao: string, concluida: boolean } = { tarefa: '', descricao: '', concluida: false };
 
-  adicionarTarefa() {
-    this.tarefas.push(this.novaTarefa);
-    this.novaTarefa = { tarefa: '', descricao: '', concluida: false };
-  }
+
 }
