49cfb2feb94da9f4e35ddab2931a6474f32d8660
emillygabrielly01 <emilly.silva2@stellantis.com>
2025-02-18 13:15:46 -0300

    Refazendo a lista de Tarefas

---
+++ src/app/adicionar-tarefa/adicionar-tarefa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-tarefa',
+  standalone: true,
+  imports: [],
+  templateUrl: './adicionar-tarefa.component.html',
+  styleUrl: './adicionar-tarefa.component.scss'
+})
+export class AdicionarTarefaComponent {
+
+}
