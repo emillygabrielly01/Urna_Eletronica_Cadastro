39e384a65016b7aeae99ef7eb092ef52b6cf2cf7
emillygabrielly01 <emilly.silva2@stellantis.com>
2025-02-24 14:13:09 -0300

    implementação do Confirmar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,58 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { AdicionarTarefaComponent } from "./adicionar-tarefa/adicionar-tarefa.component";
 
-@Component({
+interface listaTarefas 
+{
+  id: number;
+  tarefa: string;
+  descricao: string;
+  concluido: boolean;
+}
+@Component
+({
   selector: 'app-root',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
 
+
+export class AppComponent 
+{
+  tarefas: listaTarefas[] = [];
+  novaTarefa: string = '';
+  novadescricao: string = '';
+ 
+
+  adicionar()   {
+    if (this.novaTarefa.trim() !== '') 
+    {
+      const novaTarefa : listaTarefas=
+      {
+        id : Date.now(),
+        tarefa : this.novaTarefa,
+        descricao: this.novadescricao,
+        concluido : false,
+      }
+      
+      this.tarefas.push(novaTarefa);
+      this.novaTarefa = '';
+    }
+  }
+
+  tarefaConcluida(index: number)
+  {
+    this.tarefas[index].concluido = !this.tarefas[index].concluido;
+    console.log(this.tarefas);
+  }
+
+  remover(index: number)
+  {
+    this.tarefas.splice(index, 1);
+    console.log(this.tarefas);
+    this.novaTarefa = '';
+  }
 
 }
